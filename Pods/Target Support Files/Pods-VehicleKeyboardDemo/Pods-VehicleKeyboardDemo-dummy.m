f3270b15780619527a332d57a4db23d2e87c3547
陈军志 <xiaozhi6156@163.com>
2018-09-13 11:59:28 +0800

    不使用Pod引用库文件，直接使用加入Source文夹中

--- Pods/Target Support Files/Pods-VehicleKeyboardDemo/Pods-VehicleKeyboardDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VehicleKeyboardDemo : NSObject
-@end
-@implementation PodsDummy_Pods_VehicleKeyboardDemo
-@end
--- Pods/Target Support Files/VehicleKeyboard-swift/VehicleKeyboard-swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VehicleKeyboard_swift : NSObject
-@end
-@implementation PodsDummy_VehicleKeyboard_swift
-@end
